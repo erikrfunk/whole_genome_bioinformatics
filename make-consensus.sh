14ee56a761dff4b539ce277131d9376c534c469b
Erik Funk <erik.funk@colorado.edu>
2018-12-01 08:09:35 -0700

    first commit

--- make-consensus.sh
+++ make-consensus.sh
@@ -1,7 +1,9 @@
-inputvcf="/data2/rosyfinches/vcf_files/rosyfinches_all_genotyped_filtered_snps.vcf.gz"
-reference="/data2/rosyfinches/HouseFinch/final.assembly.homo.fa"
-samples="/data2/rosyfinches/sample_names.txt"
-outdir="/data2/rosyfinches/consensus_sequences/"
+#! /bin/bash
+
+inputvcf="/data2/PracticeGenomes/vcf_files/merged_vcf_files/minimum_set_filter3.recode.vcf.gz"
+reference="/data2/PracticeGenomes/Gfortis_genome/GeoFor_1.0_genomic.fna"
+samples="/data2/PracticeGenomes/cfc1.txt"
+outdir="/data2/PracticeGenomes/consensus_seqs/"
 
 while read -r samp; do
 echo "writing consensus for sample" $samp
--- per-individual-allele-freqs.sh
+++ per-individual-allele-freqs.sh
@@ -1,7 +1,10 @@
-InputDir="/data2/rosyfinches/vcf_files/"
-OutDir="/data2/rosyfinches/vcf_files/"
-SampleNames="/data2/rosyfinches/sample_names/sample_names.txt"
-InVCF="25percentmissing_and_depthfilters.recode.vcf"
+#!/bin/bash
+
+InputDir="/data2/rosyfinches/vcf_files/0p/"
+OutDir="/data2/rosyfinches/vcf_files/0p/"
+OutfilePrefix="0p_AUS"
+SampleNames="/data2/rosyfinches/sample_names/sample_names_BCRF.txt"
+InVCF="0p_AUS_snps_only-variants_biallelic.vcf"
 
 
 while read -r SAMP; do
@@ -10,7 +13,7 @@ vcftools --vcf $InputDir$InVCF --indv $SAMP --freq --out $SAMP"_temp_allele_freq
 
 awk 'FNR>1 && $3<3 {print $5}' $SAMP"_temp_allele_freqs.frq" > $SAMP"_temp_only_alleles"
 
-sed s/[ACTG]:// $SAMP"_temp_only_alleles" | tr '\n' ',' | sed s/^/$SAMP","/ | sed s/','$/'\n'/ >> $OutDir"All_allele_freq_table.csv"
+sed s/[ACTG]:// $SAMP"_temp_only_alleles" | tr '\n' ',' | sed s/^/$SAMP","/ | sed s/','$/'\n'/ >> $OutDir$OutfilePrefix"_allele_freq_table.csv"
 
 rm $SAMP"_temp_allele_freqs.frq"
 rm $SAMP"_temp_allele_freqs.log"
