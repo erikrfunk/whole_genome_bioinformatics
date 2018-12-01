77c7fec1877577366ac2dc4fc991fdfc93bd97fc
Erik R. Funk <erikrfunk@gmail.com>
2018-12-01 08:06:20 -0700

    complete reorganization and update

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-This repository contains the shell scripts used for whole genome sequence analysis from demultiplexing to variant calls as well as a variety of other analyses related to population and phylogenomics. 
-
-Note: Most all code here is invoked in shell scripts that have not yet been formatted to take input directly. Instead, input files and directories need to be changed in the bash script - sorry
-
-
-28 May 2018 -- Currently there are two options for calling variants \
-GATK HaplotypeCaller is likely best but takes a very long time \
-Samtools mpileup is faster, and may be decent enough for at least preliminary results
+This repository contains the shell scripts used for whole genome sequence analysis from demultiplexing to variant calls as well as a variety of other analyses related to population and phylogenomics.
 
 Notes on the first iteration and a description of the pipeline on the rosy-finch data is located in rosyfinch_notes.md
+
+Note: Most all code here is invoked in shell scripts that have not yet been formatted to take input directly. Instead, input files and directories need to be changed in the bash script - sorry
+The call of any script without arguments will show a short description and the arguments list.
+
+The workflow is broken up into ## sections, with each breakpoint used for validation or to provide modularity.
+
+------------------------------------------------------------------------------------------------------
+
+Step 1: Demultiplexing \
+This step can be done with any freely available demultiplexing program you prefer. The script provided here is a simple approach based on a grep search, though could be modified fairly easily to allow mismatches. Default is to make a new directory called "fastqs" in the current directory.
+
+ARGUMENTS \
+[-f] forward read file \
+[-r] reverse read file \
+[-b] barcode file - this file should contain the barcodes in column 1, as they appear \
+in the fastq header, and the corresponding sample ID in column 2. See barcodes.txt \
+
+OPTIONAL ARGUMENTS \
+[-o] output directory - default will make new directory called 'fastqs' in current directory
+
+Step 2: Trim and Quality Control \
--- sort-barcodes.sh
+++ sort-barcodes.sh
@@ -1,19 +1,51 @@
-# Shell script to demultiplex from fastq header
+#!/bin/bash
 
-barcodes="/data2/rosyfinches/barcodes/additional_barcodes.txt"
-forward="/data2/rosyfinches/Lane4/ROFI_Lane_4_TKD180302537-W-1_HL57NCCXY_L3_1.fq"
-reverse="/data2/rosyfinches/Lane4/ROFI_Lane_4_TKD180302537-W-1_HL57NCCXY_L3_2.fq"
+# Shell script to demultiplex from illumina pair-end fastq header
 
-count=1
-while read -r MATCH; do
-echo "This line reads" $MATCH
-bc=$(awk -v counter=$count 'NR==counter {print $1}' $barcodes) 
-echo "Barcode is" $bc
-sample_ID=$(awk -v counter=$count 'NR==counter {print $2}' $barcodes)
-echo "Writing $bc to" $sample_ID"read.fastq"
+if [ $# -lt 1 ]
+  then
+    echo "Demultiplex from illumina pair-end fastq header.
+    [-f] forward read file
+    [-r] reverse read file
+    [-b] barcode file - this file should contain the barcodes in column 1, as they appear
+    in the fastq header, and the corresponding sample ID in column 2. See barcodes.txt
 
-grep --no-group-separator -A 3 "$bc" $forward >> "/data2/rosyfinches/fastqs/"$sample_ID"_read1.fastq"
-grep -A 3 --no-group-separator "$bc" $reverse >> "/data2/rosyfinches/fastqs/"$sample_ID"_read2.fastq"
+    OPTIONAL ARGUMENTS
 
-count=$[$count + 1]
-done <"$barcodes"
+    [-o] output directory - default will make new directory called 'fastqs' in current directory"
+
+  else
+    while getopts f:r:b:o:; do
+    case "${option}"
+    in
+    f) forward=${OPTARG};;
+    r) reverse=${OPTARG};;
+    b) barcodes=${OPTARG};;
+    o) outdir=${OPTARG};;
+    esac
+    done
+
+    #barcodes="/data2/rosyfinches/barcodes/additional_barcodes.txt"
+    #forward="/data2/rosyfinches/Lane4/ROFI_Lane_4_TKD180302537-W-1_HL57NCCXY_L3_1.fq"
+    #reverse="/data2/rosyfinches/Lane4/ROFI_Lane_4_TKD180302537-W-1_HL57NCCXY_L3_2.fq"
+
+
+    outdir=""${outdir:-fastqs}""
+    if [ $outdir == fastqs ]
+    then
+      mkdir fastqs
+    count=1
+    while read -r MATCH; do
+    echo "This line reads" $MATCH
+    bc=$(awk -v counter=$count 'NR==counter {print $1}' $barcodes)
+    echo "Barcode is" $bc
+    sample_ID=$(awk -v counter=$count 'NR==counter {print $2}' $barcodes)
+    echo "Writing $bc to" $sample_ID"read.fastq"
+
+    grep --no-group-separator -A 3 "$bc" $forward >> "/data2/rosyfinches/fastqs/"$sample_ID"_read1.fastq"
+    grep -A 3 --no-group-separator "$bc" $reverse >> "/data2/rosyfinches/fastqs/"$sample_ID"_read2.fastq"
+
+    count=$[$count + 1]
+    done <"$barcodes"
+
+fi
