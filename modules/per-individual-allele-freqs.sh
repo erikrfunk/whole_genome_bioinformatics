29318359ea86991d566762589a8d9b2935c056e6
Erik R. Funk <erikrfunk@gmail.com>
2019-04-18 10:34:46 -0600

    added optarg

--- modules/per-individual-allele-freqs.sh
+++ modules/per-individual-allele-freqs.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-InputDir="/data2/rosyfinches/vcf_files/no_maf_filter/"
-OutDir="/data2/rosyfinches/vcf_files/no_maf_filter/"
-OutfilePrefix="75p_no_maf_filter_AUS"
-SampleNames="/data2/rosyfinches/sample_names/sample_names_BCRF.txt"
-InVCF="75p_no_maf_AUS.recode.vcf"
+if [ $# -lt 1 ]
+  then
+    echo "Calculates per-individual allele frequencies using VCFtools.
 
+    [-i] Input VCF file
+    [-o] Output file prefix
+    [-s] Sample names - text file with one sample per line "
+
+  else
+    while getopts i:o:s: option
+    do
+    case "${option}"
+    in
+    i) input_vcf=${OPTARG};;
+    o) output_prefix=${OPTARG};;
+    s) sample_names=${OPTARG};;
+    esac
+    done
 
 while read -r SAMP; do
 
-vcftools --vcf $InputDir$InVCF --indv $SAMP --freq --out $SAMP"_temp_allele_freqs"
+vcftools --vcf $input_vcf --indv $SAMP --freq --out $SAMP"_temp_allele_freqs"
 
 awk 'FNR>1 && $3<3 {print $5}' $SAMP"_temp_allele_freqs.frq" > $SAMP"_temp_only_alleles"
 
-sed s/[ACTG]:// $SAMP"_temp_only_alleles" | tr '\n' ',' | sed s/^/$SAMP","/ | sed s/','$/'\n'/ >> $OutDir$OutfilePrefix"_allele_freq_table.csv"
+sed s/[ACTG]:// $SAMP"_temp_only_alleles" | tr '\n' ',' | sed s/^/$SAMP","/ | sed s/','$/'\n'/ >> $output_prefix"_allele_freq_table.csv"
 
 rm $SAMP"_temp_allele_freqs.frq"
 rm $SAMP"_temp_allele_freqs.log"
 rm $SAMP"_temp_only_alleles"
 
-done<"$SampleNames"
+done<"$sample_names"
