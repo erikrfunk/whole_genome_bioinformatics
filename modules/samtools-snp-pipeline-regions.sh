b6bc7299d0771c9ecc42c953f09cc7903c85dd6e
Erik R. Funk <erikrfunk@gmail.com>
2020-01-16 13:43:08 -0700

    added the regions option

---
+++ modules/samtools-snp-pipeline-regions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Call snps in samtools
+
+ref="/data2/rosyfinches/HouseFinch/final.assembly.homo.fa"
+bamdir="/data2/rosyfinches/sorted_bam_files/"
+regions="/data5/meadowlarks/scaffolds1-2000.txt"
+ID="rosyfinches" # This will be used as a prefix for the output file
+
+echo "making a pileup file for" $ID >> log
+#can also add the -R flag joined with a scaffold list to subset and parralel
+bcftools mpileup -Ou -f $ref--ignore-RG --regions-file $regions -a AD,ADF,DP,SP,INFO/AD,INFO/ADF \
+"$bamdir"*.bam | bcftools call -mv > "$ID"_raw_variants.vcf
+#echo "removing all lines with two comment marks" >> log
+#grep -v "##" "$ID"_snps_indels.vcf > "$ID"_snps_indels_short.vcf
+echo "filtering low quality snps (<100)" >> log
+awk '$1~/^#/ || $6 > 100 {print $0}' > \
+"$ID"_snps_indels_filtered.vcf "$ID"_snps_indels_short.vcf
+echo "add the header and check length of column 4 and 5 to make sure
+they are snp type variants" >> log
+awk '$1~/^#/ || length($4)==1 && length($5)==1 {print $0}'> \
+"$ID"_snps_filtered.vcf "$ID"_snps_indels_filtered.vcf
